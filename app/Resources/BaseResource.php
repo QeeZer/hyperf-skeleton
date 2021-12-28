b49b8267a147e2a761d03d2339914f4266f4e43b
qeezer <184353381@qq.com>
2021-12-28 17:07:58 +0800

    install hyperf framework.

--- app/Resources/BaseResource.php
+++ app/Resources/BaseResource.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Resources;
 
 use QeeZer\HyperfApiResponder\DefaultResource;
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 /**
  * This file is part of Hyperf.
  *
- * @link     https://www.hyperf.io
+ * @see     https://www.hyperf.io
  * @document https://hyperf.wiki
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
